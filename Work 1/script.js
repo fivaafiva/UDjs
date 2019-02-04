f303f7e8805af74efeb27df227e6c2cd5f5a5dd7
fivaafiva <andrey.reznikov96@gmail.com>
2019-02-04 15:16:39 +0500

    Решение задачи #2

--- Work 1/script.js
+++ Work 1/script.js
@@ -1,19 +1,70 @@
 'use strict';
-
-let q1, q2, budjet = 5000, /*+prompt('Ваш бюджет на месяц');*/
-  time = "2018-05-21",/*prompt('Введите дату в формате YYYY-MM-DD');*/
-  appData = {
-  budjetData: budjet,
-  timeData: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: Array(),
-  saving: false
+//Узнаем бюджет и дату
+let budjet = 5000, /*+prompt('Ваш бюджет на месяц');*/
+    time = "2018-05-21";/*prompt('Введите дату в формате YYYY-MM-DD');*/
+//Объект с данными
+let appData = {
+    budjetData: budjet,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: Array(),
+    saving: false
 };
+//Цикл и проверка статей расходов
 for (let i = 0; i < 2; i++) {
-   q1 = prompt('Введите обязательную статью расходов в этом месяце');
-   q2 = prompt('Во сколько обойдется?');
-   appData.expenses.q1 = q2;
-} 
-//appData.expenses.q1 = ; 
-console.log('бюджет на 1 день из 30 = ' + (appData.budjetData / 30).toFixed(2) + 'у.е');
+  let q1 = prompt('Введите обязательную статью расходов в этом месяце'),
+      q2 = prompt('Во сколько обойдется?');
+  if ( (typeof(q1) != null) && (typeof(q2) != null) &&
+      (q1 != '') &&  (q2 != '') && (q1.length < 20)  ) {
+      console.log('done #' + i);
+      appData.expenses[q1] = q2;
+  } else {
+    alert('Вы не првильно ввели данные о расходах');
+    i--;
+  }
+}
+//Тренировка циклов
+/*
+let i = 0;
+//Цикл While
+while (i < 2) {
+  let q1 = prompt('Введите обязательную статью расходов в этом месяце'),
+      q2 = prompt('Во сколько обойдется?');
+  i++;
+  if ( (typeof(q1) != null) && (typeof(q2) != null) &&
+      (q1 != '') &&  (q2 != '') && (q1.length < 20)  ) {
+      console.log('done #' + i);
+      appData.expenses[q1] = q2;
+  } else {
+    alert('Вы не првильно ввели данные о расходах');
+    i--;
+  }
+}
+do {
+  let q1 = prompt('Введите обязательную статью расходов в этом месяце'),
+      q2 = prompt('Во сколько обойдется?');
+  i++;
+  if ( (typeof(q1) != null) && (typeof(q2) != null) &&
+      (q1 != '') &&  (q2 != '') && (q1.length < 20)  ) {
+      console.log('done #' + i);
+      appData.expenses[q1] = q2;
+  } else {
+    alert('Вы не првильно ввели данные о расходах');
+    i--;
+  }
+} while (i < 2);
+*/
+//Записываем бюджет на один день в объект    
+appData.moneyPerDay = (appData.budjetData / 30).toFixed(2); 
+console.log('Бюджет на 1 день из 30: ' + appData.moneyPerDay + 'у.е');
+//Проверяем уровень достатка
+if (appData.moneyPerDay < 100 ) {
+  console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('Высокий уровень достатка');
+} else {
+  console.log('Произошла ошибка');
+}
